9cfbf297b780b8e876d5b452bfaa0b86503b6b2c
Sebastian-ivaniczki <sivaniczki@gmail.com>
2023-01-24 16:24:32 +0100

    add main and axios for api

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,33 @@
 <script>
+import axios from 'axios';
+import { store } from './dta/store'
 import AppHeader from './components/AppHeader.vue';
+import AppMain from './components/AppMain.vue';
 export default {
- components: { AppHeader }
+ components: { AppHeader, AppMain },
+  data() {
+    return { store }
+  },
+  methods: {
+    fetchPokemons() {
+      axios.get('https://41tyokboji.execute-api.eu-central-1.amazonaws.com/dev/api/v1/pokemons?per=10&page=1')
+      .then(res => {
+        store.pokemons = res.data.docs
+      })
+    }
+   
+  }, 
+  created() {
+      this.fetchPokemons()
+    }
+
 };
 
 </script>
 
 <template>
   <app-header></app-header>
+  <app-main></app-main>
 </template>
 
 <style lang="scss">
---
+++ src/components/AppMain.vue
@@ -0,0 +1,25 @@
+<script>
+export default {
+   name: 'AppMain' 
+   
+}
+</script>
+
+<template>
+<div class="container">
+    <div class="row g-5 row-cols-2 row-cols-md-3 row-cols-xl-5 ">
+        <div class="my-3 pokemon-card">
+            <img src="" alt="">
+        </div>
+    </div>
+</div>
+</template>
+
+<style lang="scss">
+ .pokemon-card{
+    height: 300px;
+    background-color: aquamarine;
+    border-radius: 10px;
+    padding: 10px 5px 10px 5px;
+ }
+</style>
